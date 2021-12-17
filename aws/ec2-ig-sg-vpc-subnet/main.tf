272b36031e9b37bafe75376034e6ee39d0a42ace
felipedds <53063266+felipedds@users.noreply.github.com>
2021-12-17 08:09:59 -0300

    Update main.tf

--- aws/ec2-ig-sg-vpc-subnet/main.tf
+++ aws/ec2-ig-sg-vpc-subnet/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAR7AZUAOQAZGYRL7X"
-  secret_key = "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"
+  access_key = "access_key"
+  secret_key = "secret_key"
 }
