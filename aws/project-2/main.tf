b97a21af04e316882b4034eba961cf521fe54195
Felipe Dias de Souza <53063266+felipedds@users.noreply.github.com>
2022-12-18 18:05:16 -0300

    add files

--- aws/project-2/main.tf
+++ aws/project-2/main.tf
@@ -9,3 +9,34 @@ module "subnet" {
   vpc_id            = module.vpc.vpc_id
   subnet_cidr_block = var.subnet_cidr_block
 }
+
+module "internet_gateway" {
+  source = "./modules/aws_internet_gateway"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "route_table" {
+  source              = "./modules/aws_route_table"
+  subnet_id           = module.subnet.subnet_id
+  vpc_id              = module.vpc.vpc_id
+  internet_gateway_id = module.internet_gateway.internet_gateway_id
+}
+
+module "security_group" {
+  source              = "./modules/aws_security_group"
+  vpc_id              = module.vpc.vpc_id
+  security_group_tags = var.security_group_tags
+}
+
+module "key_pair" {
+  source = "./modules/aws_key_pair"
+}
+
+module "ec2" {
+  source            = "./modules/aws_ec2"
+  key_pair_id       = module.key_pair.key_pair_id
+  ec2_tags          = var.ec2_tags
+  subnet_id         = module.subnet.subnet_id
+  security_group_id = module.security_group.security_group_id
+}
+
--- aws/project-2/variables.tf
+++ aws/project-2/variables.tf
@@ -7,7 +7,7 @@ variable "vpc_cidr_block" {
 variable "vpc_tags" {
   type = map(any)
   default = {
-    Name = "my_vpc"
+    Name = "mtc_vpc"
   }
 }
 
@@ -20,6 +20,38 @@ variable "subnet_cidr_block" {
 variable "subnet_tags" {
   type = map(any)
   default = {
-    "Name" = "my_vpc"
+    "Name" = "mtc_subnet"
+  }
+}
+
+# Internet Gateway
+variable "internet_gateway_tags" {
+  type = map(any)
+  default = {
+    "Name" = "mtc_internet_gateway"
+  }
+}
+
+# Route Table
+variable "route_table_tags" {
+  type = map(any)
+  default = {
+    "Name" = "mtc_route_table"
+  }
+}
+
+# Security Group
+variable "security_group_tags" {
+  type = map(any)
+  default = {
+    "Name" = "mtc_security_group"
+  }
+}
+
+# EC2
+variable "ec2_tags" {
+  type = map(any)
+  default = {
+    "Name" = "mtc_ec2"
   }
 }
